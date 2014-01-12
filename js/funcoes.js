bbb95df13022d159cb6d5357338ccae2d6b9d5e0
marasi <mauricio.imbe@gmail.com>
2014-01-12 14:04:24 -0200

    002

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,10 +1,11 @@
 function ajaxBairros() {
     $("#cidade").change(function() {
-	
+	  
 	  var id = $(this).children(":selected").attr("id");
-
+		
 	  $.ajax({
-		  url:"PHP/inBairros.php?idCidade="+id, 
+		  url:"PHP/inBairros.php?idCidade="+id,
+		  async:"true", 
 		  type:"GET", 
 		  success: function(result){
 			  $("#bairros").html(result);
