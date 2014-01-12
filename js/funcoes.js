323c96a1777b368c428180923ab149e0dcc4ff53
marasi <mauricio.imbe@gmail.com>
2014-01-12 17:39:59 -0200

    005

--- js/funcoes.js
+++ js/funcoes.js
@@ -18,6 +18,32 @@ function ajaxBairros() {
 	});
 }
 
+function pesquisar(){
+	
+	var bT = $("#tipo").val();
+	var bM = $("#modalidade").val();
+	var bL = $("#cidade").val();
+	if($("#bairro"))var bB = $("#bairro").val();
+	var bV = $("#busca_valor").val();
+	var bC = $("#busca_ci").val();
+	
+ 	$.ajax({
+		  url:URL+"/app/PHP/result.php",
+		  data: "{'bT':'" + bT+ "', 'bM':'" + bM+ "', 'bL':'" + bL+ "', 'bB':'" + bB+ "', 'bV':'" + bV+ "', 'bC':'" + bC+ "'}",
+		  async:"true", 
+		  type:"POST", 
+		  success: function(result){
+			  $("#buscaResult").html(result);
+			  $(document).listview().trigger("create"); /*refresh css*/
+		  }, 
+		  error: function (xhr, status, error) {
+				alert("ERRO: ajax!");
+    	  }
+	  });
+	  return false;
+	  event.preventDefault();	
+}
+
 
  
 
