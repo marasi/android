263da31024145adb6c3f847de3f55f1fd52f1dee
marasi <mauricio.imbe@gmail.com>
2014-01-30 16:48:09 -0200

    ok

--- js/share.js
+++ js/share.js
@@ -1,7 +1,7 @@
 function shareS(){
-	window.plugins.socialsharing.share(TITULO, 'Site Imobiliária', null, URL);
+	window.plugins.socialsharing.share(TITULO, 'Site Imobiliária: <br>', null, '<a url="'+URL+'">'+URL+'"</a>');
 }
-function shareM(){
+function shareI(){
 	var id = window.name; //id storaged
-	window.plugins.socialsharing.share(TITULO, 'Site Imobiliária', null, URL+"/index.php?pagina=ver&id="+id);
+	window.plugins.socialsharing.share(TITULO, 'Site Imobiliária: <br>', null, '<a url="'+URL+"/index.php?pagina=ver&id="+id+'">'+URL+"/index.php?pagina=ver&id="+id+'"</a>');
 }
