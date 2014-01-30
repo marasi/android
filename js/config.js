4efcd4c19e6dbebb56981114167becaea514c9da
marasi <mauricio.imbe@gmail.com>
2014-01-30 18:41:40 -0200

    everything works!

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 var URL = "http://www.imoveispraiars.com.br"; /*http://www.nomedosite.com.br*/
-var TITULO = "Imóveis Praia Rs";
+var TITULO = "Imóveis Praia Rs"; /*to share it*/
--- js/share.js
+++ js/share.js
@@ -2,6 +2,7 @@ function shareS(){
 	window.plugins.socialsharing.share(TITULO, 'Site Imobiliária', null, URL);
 }
 function shareI(){
-	var id = window.name; //id storaged
+	//var id = window.name; //id storaged
+	alert(id);
 	window.plugins.socialsharing.share(TITULO, 'Site Imobiliária', null, URL+"/index.php?pagina=ver&id="+id);
 }
