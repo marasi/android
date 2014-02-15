bb7f29842a332ac4c3b4424609160212bc6e9efe
marasi <mauricio.imbe@gmail.com>
2014-02-15 12:01:14 -0200

    true

--- js/funcoes.js
+++ js/funcoes.js
@@ -13,8 +13,8 @@ function checkConn() {
 		}else{
 			//$.mobile.changePage($('#menu'), 'pop');
 			history.back();
-			document.removeEventListener("backbutton", function (e) {
-				e.preventDefault();
+			document.addEventListener("backbutton", function (e) {
+				return true;
 			}, false );
 		}		
 	}
